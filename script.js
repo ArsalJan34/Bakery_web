213bbae7e08a9bec9e3cd597836639d7835d83de
ArsalJan34 <arsaljaan532@gmail.com>
2025-11-08 22:12:20 +0500

    Changes made

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  function showSidebar() {
+    document.querySelector('.sidebar').classList.add('show');
+  }
+  function hideSidebar() {
+    document.querySelector('.sidebar').classList.remove('show');
+  }
+
